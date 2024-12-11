f3be3cf5bc8a368e83c9c8b98432ed2ae205dafd
j.sonar <j.sonar@direction.biz>
2024-12-11 16:35:05 +0530

    first commit

---
+++ src/main/java/com/example/java_helloworld/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.java_helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/app")
+    public String getMessage() {
+        return "Hello World";
+    }
+}
